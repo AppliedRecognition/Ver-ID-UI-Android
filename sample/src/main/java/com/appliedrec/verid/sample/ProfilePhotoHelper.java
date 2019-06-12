78d3add2009af5da46ba43696edfae8e001e89b3
Jakub Dolejs <jakubdolejs@gmail.com>
2019-06-12 20:04:45 +0100

    Fixed cropping of result images from front camera

--- sample/src/main/java/com/appliedrec/verid/sample/ProfilePhotoHelper.java
+++ sample/src/main/java/com/appliedrec/verid/sample/ProfilePhotoHelper.java
@@ -3,6 +3,8 @@ package com.appliedrec.verid.sample;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.net.Uri;
 import android.os.AsyncTask;
 import androidx.exifinterface.media.ExifInterface;
@@ -27,7 +29,7 @@ class ProfilePhotoHelper {
         imageUtils = new ImageUtils(this.context);
     }
 
-    void setProfilePhotoUri(final Uri imageUri) {
+    void setProfilePhotoUri(final Uri imageUri, final RectF cropRect) {
         synchronized (profilePhotoLock) {
             while (isSettingProfilePhoto) {
                 try {
@@ -47,6 +49,15 @@ class ProfilePhotoHelper {
                         byte[] grayscale = imageUtils.bitmapToGrayscale(bitmap, ExifInterface.ORIENTATION_NORMAL);
                         Bitmap grayscaleBitmap = imageUtils.grayscaleToBitmap(grayscale, bitmap.getWidth(), bitmap.getHeight());
                         bitmap.recycle();
+                        if (cropRect != null) {
+                            Rect crop = new Rect();
+                            cropRect.round(crop);
+                            crop.left = Math.max(crop.left,0);
+                            crop.top = Math.max(crop.top,0);
+                            crop.right = Math.min(crop.right,grayscaleBitmap.getWidth());
+                            crop.bottom = Math.min(crop.bottom,grayscaleBitmap.getHeight());
+                            grayscaleBitmap = Bitmap.createBitmap(grayscaleBitmap, crop.left, crop.top, crop.width(), crop.height());
+                        }
                         OutputStream outputStream = context.getContentResolver().openOutputStream(profilePhotoUri);
                         grayscaleBitmap.compress(Bitmap.CompressFormat.JPEG, 95, outputStream);
                     }
