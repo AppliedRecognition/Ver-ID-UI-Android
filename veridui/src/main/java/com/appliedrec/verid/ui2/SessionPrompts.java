9f3dca1654c533ea399cb8a05fc7156f0ece6c7f
Jakub Dolejs <jakubdolejs@gmail.com>
2020-06-25 14:44:07 +0100

    Added helper class that generates session prompt text based on face detection result

---
+++ veridui/src/main/java/com/appliedrec/verid/ui2/SessionPrompts.java
@@ -0,0 +1,33 @@
+package com.appliedrec.verid.ui2;
+
+import com.appliedrec.verid.core2.session.FaceDetectionResult;
+
+import java.util.Locale;
+import java.util.Optional;
+
+public class SessionPrompts {
+
+    private final IStringTranslator stringTranslator;
+
+    public SessionPrompts(IStringTranslator stringTranslator) {
+        this.stringTranslator = stringTranslator;
+    }
+
+    public Optional<String> promptFromFaceDetectionResult(FaceDetectionResult faceDetectionResult) {
+        switch (faceDetectionResult.getStatus()) {
+            case FACE_FIXED:
+            case FACE_ALIGNED:
+                return Optional.of(stringTranslator.getTranslatedString("Great, hold it"));
+            case FACE_MISALIGNED:
+                return Optional.of(stringTranslator.getTranslatedString("Slowly turn to follow the arrow"));
+            case FACE_TURNED_TOO_FAR:
+                return Optional.empty();
+            default:
+                return Optional.of(stringTranslator.getTranslatedString("Align your face with the oval"));
+        }
+    }
+
+    public Locale getLocale() {
+        return stringTranslator.getLocale();
+    }
+}
