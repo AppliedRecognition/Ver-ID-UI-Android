b6c8d742eb09619f5b8a9c662fff4bf04f9c597b
Jakub Dolejs <jakubdolejs@gmail.com>
2023-02-21 12:29:26 +0000

    Simplified build scripts using a plugin

---
+++ buildSrc/src/main/java/com/appliedrec/VerIDVersionExtension.kt
@@ -0,0 +1,35 @@
+package com.appliedrec
+
+open class VerIDVersionExtension {
+
+    val versionMajor = 2
+    val versionMinor = 8
+    val versionPatch = 0
+    val versionClassifier: String? = null
+    val versionClassifierVersion: String? = null
+
+    val minSdkVersion = 24
+    val targetSdkVersion = 33
+    val buildToolsVersion = "30.0.3"
+    val compileSdkVersion = 33
+    val kotlinCompilerExtensionVersion = "1.4.2"
+    val kotlinJvmTarget = "11"
+    val kotlinVersion = "1.8.10"
+
+    val versionCode: Int
+        get() = minSdkVersion * 10000000 + versionMajor * 10000 + versionMinor * 100 + versionPatch
+
+    val versionName: String
+        get() {
+            val versionName = StringBuilder()
+            versionName.append(versionMajor).append(".").append(versionMinor).append(".")
+                .append(versionPatch)
+            if (versionClassifier != null) {
+                versionName.append("-").append(versionClassifier)
+                if (versionClassifierVersion != null) {
+                    versionName.append(".").append(versionClassifierVersion)
+                }
+            }
+            return versionName.toString()
+        }
+}
