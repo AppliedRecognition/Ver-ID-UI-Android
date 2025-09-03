20e90450b0c430f6fc6a496bd889ec66a4088ce4
jihanurrahman33 <jihanurrahman33@gmail.com>
2025-09-04 03:33:56 +0600

    Chore: Make translation XML generator Python 3 friendly with main guard

--- translation_xml.py
+++ translation_xml.py
@@ -1,10 +1,23 @@
 from translations import strings
 
-words = strings()
-xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\
-<strings>\n"
-for word in words:
-    xml += "    <string>\n        <original>"+word+"</original>\n        <translation></translation>\n    </string>\n"
-xml += "</strings>"
 
-print(xml)
+def main():
+    words = strings()
+    xml = (
+        "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+        "<strings>\n"
+    )
+    for word in words:
+        xml += (
+            "    <string>\n"
+            f"        <original>{word}</original>\n"
+            "        <translation></translation>\n"
+            "    </string>\n"
+        )
+    xml += "</strings>"
+
+    print(xml)
+
+
+if __name__ == "__main__":
+    main()
